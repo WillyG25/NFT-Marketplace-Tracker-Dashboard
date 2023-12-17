32e496a9b08f2d0c06ecf35b918930233406af27
WillyG25 <133692794+WillyG25@users.noreply.github.com>
2023-12-17 03:46:16 +0000

    Update database_manager.py

--- database_manager.py
+++ database_manager.py
@@ -1,27 +1,47 @@
 import mysql.connector
 
+import mysql.connector
+
 class DatabaseManager:
     def __init__(self, host, user, password, database):
-        self.connection = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database
-        )
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database
+            )
+            self.cursor = self.connection.cursor()
+            print("Connected to the database successfully.")
+        except mysql.connector.Error as err:
+            print(f"Error during database connection: {err}")
+            raise  # Re-raise the exception after printing the error message
 
     def execute_query(self, query, values=None):
-        if values:
-            self.cursor.execute(query, values)
-        else:
-            self.cursor.execute(query)
-        self.connection.commit()
+        try:
+            if values:
+                self.cursor.execute(query, values)
+            else:
+                self.cursor.execute(query)
+            self.connection.commit()
+        except mysql.connector.Error as err:
+            print(f"Error executing query: {err}")
+            raise
 
     def fetch_one(self, query):
-        self.cursor.execute(query)
-        return self.cursor.fetchone()
+        try:
+            self.cursor.execute(query)
+            return self.cursor.fetchone()
+        except mysql.connector.Error as err:
+            print(f"Error fetching one result: {err}")
+            raise
 
     def fetch_all(self, query):
-        self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        return result
+        try:
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+        except mysql.connector.Error as err:
+            print(f"Error fetching all results: {err}")
+            raise
+
